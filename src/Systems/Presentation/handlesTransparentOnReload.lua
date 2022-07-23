feec20318a29e8f17a11fdac0f856e512eb8e383
ThegameboySP <delfinocorner@gmail.com>
2022-07-23 19:55:31 -0400

    Fix handles transparent on reload

--- src/Systems/Presentation/handlesTransparentOnReload.lua
+++ src/Systems/Presentation/handlesTransparentOnReload.lua
@@ -2,14 +2,10 @@ local Priorities = require(script.Parent.Parent.Priorities)
 local Components = require(script.Parent.Parent.Parent.Components)
 
 local function handlesTransparentOnReload(world, params)
-    for id, toolRecord in world:queryChanged(Components.Tool) do
-        if toolRecord.new and toolRecord.new.isEquipped and toolRecord.old and not toolRecord.old.isEquipped then
-            local part = world:get(id, Components.Part)
-
-            if part then
-                local timeLeft = toolRecord.new.nextReloadTimestamp - params.currentFrame
-                part.part.Transparency = (timeLeft > 0) and 0.4 or 0
-            end
+    for _id, part, tool in world:query(Components.Part, Components.Tool, Components.Owned):without(Components.SwordTool) do
+        if tool.isEquipped then
+            local timeLeft = tool.nextReloadTimestamp - params.currentFrame
+            part.part.Transparency = (timeLeft > 0) and 0.4 or 0
         end
     end
 end
